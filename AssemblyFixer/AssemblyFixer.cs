5066c1033c08762945fc379cf65fabdfb61d2f27
文煌 <wwh1004@hotmail.com>
2019-08-03 10:18:34 +0800

    update AssemblyFixer

--- AssemblyFixer/AssemblyFixer.cs
+++ AssemblyFixer/AssemblyFixer.cs
@@ -1,26 +1,64 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Mdlib.PE;
 
 namespace UniversalDotNetTools {
 	/// <summary>
 	/// Assembly fixer
 	/// </summary>
 	public static class AssemblyFixer {
-		private static readonly IFixer[] _fixers;
+		private static IFixer[] _fixers;
 
 		/// <summary>
 		/// Internal fixers
 		/// </summary>
-		public static IFixer[] Fixers => _fixers;
+		public static IFixer[] Fixers {
+			get {
+				if (_fixers is null) {
+					Type fixerType;
 
-		static AssemblyFixer() {
-			Type fixerType;
-
-			fixerType = typeof(IFixer);
-			_fixers = fixerType.Module.GetTypes().Where(t => t.GetInterfaces().Any(i => i.IsAssignableFrom(fixerType))).Select(t => (IFixer)Activator.CreateInstance(t)).ToArray();
+					fixerType = typeof(IFixer);
+					_fixers = fixerType.Module.GetTypes().Where(t => t.GetInterfaces().Any(i => i.IsAssignableFrom(fixerType))).Select(t => (IFixer)Activator.CreateInstance(t)).ToArray();
+				}
+				return _fixers;
+			}
 		}
 
+		/// <summary>
+		/// Check errors
+		/// </summary>
+		/// <param name="peImage"></param>
+		/// <returns></returns>
+		public static IDictionary<IFixer, FixerMessage> Check(IPEImage peImage) {
+			Dictionary<IFixer, FixerMessage> messages;
+
+			messages = new Dictionary<IFixer, FixerMessage>();
+			foreach (IFixer fixer in Fixers) {
+				FixerMessage message;
+
+				if (fixer.Check(peImage, out message))
+					messages.Add(fixer, message);
+			}
+			return messages;
+		}
+
+		/// <summary>
+		/// Fix errors
+		/// </summary>
+		/// <param name="peImage"></param>
+		/// <returns></returns>
+		public static IDictionary<IFixer, FixerMessage> Fix(IPEImage peImage) {
+			Dictionary<IFixer, FixerMessage> messages;
+
+			messages = new Dictionary<IFixer, FixerMessage>();
+			foreach (IFixer fixer in Fixers) {
+				FixerMessage message;
+
+				if (fixer.Fix(peImage, out message))
+					messages.Add(fixer, message);
+			}
+			return messages;
+		}
 	}
 }
--- AssemblyFixer/IFixer.cs
+++ AssemblyFixer/IFixer.cs
@@ -1,3 +1,4 @@
+using System;
 using Mdlib.PE;
 
 namespace UniversalDotNetTools {
@@ -22,6 +23,45 @@ namespace UniversalDotNetTools {
 	}
 
 	/// <summary>
+	/// Fixer message
+	/// </summary>
+	public sealed class FixerMessage {
+		private readonly FixerLevel _level;
+		private readonly string _text;
+
+		/// <summary>
+		/// Level
+		/// </summary>
+		public FixerLevel Level => _level;
+
+		/// <summary>
+		/// Text
+		/// </summary>
+		public string Text => _text;
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="level"></param>
+		/// <param name="text"></param>
+		public FixerMessage(FixerLevel level, string text) {
+			switch (level) {
+			case FixerLevel.None:
+			case FixerLevel.Warning:
+			case FixerLevel.Error:
+				break;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(level));
+			}
+			if (text is null)
+				throw new ArgumentNullException(nameof(text));
+
+			_level = level;
+			_text = text;
+		}
+	}
+
+	/// <summary>
 	/// A fixer interface
 	/// </summary>
 	public interface IFixer {
@@ -34,10 +74,9 @@ namespace UniversalDotNetTools {
 		/// Check errors
 		/// </summary>
 		/// <param name="peImage"></param>
-		/// <param name="level"></param>
 		/// <param name="message"></param>
 		/// <returns></returns>
-		bool Check(IPEImage peImage, out FixerLevel level, out string message);
+		bool Check(IPEImage peImage, out FixerMessage message);
 
 		/// <summary>
 		/// Fix errors
@@ -45,6 +84,6 @@ namespace UniversalDotNetTools {
 		/// <param name="peImage"></param>
 		/// <param name="message"></param>
 		/// <returns></returns>
-		bool Fix(IPEImage peImage, out string message);
+		bool Fix(IPEImage peImage, out FixerMessage message);
 	}
 }
